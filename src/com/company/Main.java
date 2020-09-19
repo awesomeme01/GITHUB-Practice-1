d785ee51f6061456f53369112fb71fa41075ee4a
awesomeme01 <shabdan.abjaparov.official@gmail.com>
2020-09-19 18:55:48 +0600

    Second commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String students = // 1) создайте массив из десяти людей
+        for(int i = 0; i<students._____ ; i++){// 2) вместо пустоты вызовите функцию возвращающую длину массива
+            //3) напишите код который будет принимать имена от юзера(проверять на валидность не надо)
+        }
+        Random rand = new Random();
+        int n = rand.nextInt(students._____);// 4) тоже самое что в шаге 2
+        //5) допишите loop который будет принтить всё кроме обьекта из массива номер которого выдал рандом которую выдает Random
+        //ваш код...
+
+
+
+
+
+        //push and commit в свой branch в GITHUB Practice 1
+    }
+}
